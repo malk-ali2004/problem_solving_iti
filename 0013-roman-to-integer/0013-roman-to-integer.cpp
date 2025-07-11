9aef9b987b6f2e997aae943c2225674433eda67a
malk-ali2004 <ma6661@fayoum.edu.eg>
2025-07-11 18:13:18 +0300

    Time: 2 ms (77.33%), Space: 9.9 MB (93.42%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        int dec = 0;
+        int len = s.length();
+        for (int i = 0; i < len; i++) {
+            if (s[i] == 'I') {
+                if (i + 1 < len && s[i + 1] == 'V') {
+                    dec += 4;
+                    i++;
+                } else if (i + 1 < len && s[i + 1] == 'X') {
+                    dec += 9;
+                    i++;
+                } else {
+                    dec += 1;
+                }
+            } else if (s[i] == 'X') {
+                if (i + 1 < len && s[i + 1] == 'L') {
+                    dec += 40;
+                    i++;
+                } else if (i + 1 < len && s[i + 1] == 'C') {
+                    dec += 90;
+                    i++;
+                } else {
+                    dec += 10;
+                }
+            } else if (s[i] == 'C') {
+                if (i + 1 < len && s[i + 1] == 'D') {
+                    dec += 400;
+                    i++;
+                } else if (i + 1 < len && s[i + 1] == 'M') {
+                    dec += 900;
+                    i++;
+                } else {
+                    dec += 100;
+                }
+            } else if (s[i] == 'V') {
+                dec += 5;
+            } else if (s[i] == 'L') {
+                dec += 50;
+            } else if (s[i] == 'D') {
+                dec += 500;
+            } else if (s[i] == 'M') {
+                dec += 1000;
+            }
+        }
+        return dec;
+    }
+};
