dc74214a54d9f71fef96345a79472433051aa1be
malk-ali2004 <ma6661@fayoum.edu.eg>
2025-07-11 18:15:26 +0300

    Time: 163 ms (77.58%), Space: 12.8 MB (44.6%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -1,6 +1,33 @@
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int n,count = 0;
+//     cout << "Enter a number: ";
+//     cin >> n;
+
+//     for (int i = 2; i < n; i++) {
+//         bool isPrime = true;
+
+//         for (int j = 2; j * j <= i; j++) {
+//             if (i % j == 0) {
+//                 isPrime = false;
+//                 break;
+//             }
+//         }
+
+//         if (isPrime) {
+//             count++;
+//         }
+//     }
+
+//     cout << count << endl;
+//     return 0;
+// }
 class Solution {
 public:
     int countPrimes(int n) {
+        
         if (n <= 2) return 0;
 
         vector<bool> isPrime(n, true);
