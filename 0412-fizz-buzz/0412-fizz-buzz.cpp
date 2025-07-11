ea348b4584599c4aac5a554d574f66f7f1849b8f
malk-ali2004 <ma6661@fayoum.edu.eg>
2025-07-11 17:54:36 +0300

    Time: 1 ms (53.45%), Space: 11.7 MB (43.89%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<string> fizzBuzz(int num) {
+         vector<string> ans;
+
+        for (int i = 1; i <= num; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                ans.push_back("FizzBuzz");
+            } else if (i % 3 == 0) {
+                ans.push_back("Fizz");
+            } else if (i % 5 == 0) {
+                ans.push_back("Buzz");
+            } else {
+                ans.push_back(to_string(i));
+            }
+        }
+
+        return ans;
+        
+    }
+};
